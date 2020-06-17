cfe7089f6f626ee76ab8e2f321dc70decf21b483
yuukidach <chendamailbox@foxmail.com>
2020-06-17 22:27:51 +0800

    pass 1014

---
+++ Q1014_BestSightseeingPair.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& A) {
+        int ans = 0, mx = A[0] + 0;
+        for (int j = 1; j < A.size(); ++j) {
+            ans = max(ans, mx + A[j] - j);
+            mx = max(mx, A[j] + j);
+        }
+        return ans;
+    }
+};
