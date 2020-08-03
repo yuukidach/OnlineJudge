2001dde8cf6a276b0a06ffb48822a4bb9e9f4a14
yuukidach <chendamailbox@foxmail.com>
2020-08-03 19:56:21 +0800

    pass 714

---
+++ Q714_BestTimetoBuyandSellStockwithTransactionFee.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int days = prices.size();
+        int dp[days+1][2];
+
+        dp[0][0] = 0;
+        dp[0][1] = INT_MIN + fee;
+
+        for (int i = 1; i <= days; ++i) {
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i-1] - fee);
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i-1]);
+        }
+
+        return dp[days][0];
+    }
+};
