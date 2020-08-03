d18982f846f2cb9696b5f395271aedce595755f0
yuukidach <chendamailbox@foxmail.com>
2020-08-03 20:21:10 +0800

    update 198

--- Q198_HouseRobber.cpp
+++ Q198_HouseRobber.cpp
@@ -3,25 +3,16 @@ public:
     int rob(vector<int>& nums) {
         int len = nums.size();
         if (len == 0) return 0;
-        if (len == 1) return nums[0];
-        if (len == 2) return max(nums[0], nums[1]);
-        if (len == 3) return max(nums[0]+nums[2], nums[1]);
-        int dp[len];
-        memset(dp, 0, sizeof(int) * len);
 
-        int res = INT_MIN;
+        int dp[len+1];
 
-        dp[0] = nums[0];
-        dp[1] = nums[1];
-        dp[2] = nums[0] + nums[2];
-        res = max(dp[0], dp[1]);
-        res = max(res, dp[2]);
+        dp[0] = 0;
+        dp[1] = nums[0];
 
-        for (int i = 3; i < len; ++i) {
-            dp[i] = max(dp[i-2], dp[i-3]) + nums[i];
-            res = max(res, dp[i]);
+        for (int i = 2; i <= len; ++i) {
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i-1]);
         }
 
-        return res;
+        return dp[len];
     }
 };
