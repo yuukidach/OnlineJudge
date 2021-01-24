068980cec54b12a7c27343b17dfbb578dea37f78
yuukidach <chendamailbox@foxmail.com>
2021-01-24 18:15:42 +0800

    docs: add leetcode 35

---
+++ LeetCode/binary_search/Q35_SearchInsertPosition.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, r = nums.size();
+        int mid;
+        while (l < r) {
+            mid = (l + r) >> 1;
+            if (nums[mid] < target) l = mid + 1;
+            else r = mid;
+        }
+        return l;
+    }
+};
