db1efcf295caa659edf9b3f8b4b0d983f2084c2c
yuukidach <chendamailbox@foxmail.com>
2021-02-03 22:11:34 +0800

    docs: add leetcode 480

---
+++ LeetCode/sliding_window/Q480_SlidingWindowMedian.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<double> medianSlidingWindow(vector<int>& nums, int k) {
+        vector<double> res;
+        multiset<double> st;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (st.size() >= k) st.erase(st.find(nums[i - k]));
+            st.insert(nums[i]);
+            if (i >= k - 1) {
+                auto mid = st.begin();
+                std::advance(mid, k / 2);
+                res.push_back((*mid + *prev(mid, (1 - k % 2))) / 2);
+            }
+        }
+        return res;
+    }
+};
