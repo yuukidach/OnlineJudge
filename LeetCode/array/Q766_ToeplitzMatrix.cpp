670f1eba8b829c87698465349e5ed45ed57d48f2
yuukidach <chendamailbox@foxmail.com>
2021-02-22 23:28:36 +0800

    docs: add leetcode 766

---
+++ LeetCode/array/Q766_ToeplitzMatrix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i * j != 0 && matrix[i-1][j-1] != matrix[i][j]) return false;
+            }
+        }
+
+        return true;
+    }
+};
