b89f0cd41055d48347c3388cfafeb630cef101a6
yuukidach <chendamailbox@foxmail.com>
2021-03-26 10:48:39 +0800

    docs: add leetcode 83

---
+++ LeetCode/linked_list/Q83_RemoveDuplicatesfromSortedList.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *p = head;
+        if (p == nullptr) return head;
+        while (p->next != nullptr) {
+            if (p->val == p->next->val) p->next = p->next->next;
+            else p = p->next;
+        }
+        return head;
+    }
+};
